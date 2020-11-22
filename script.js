5ecd7df8b6ea6f20f95c346d212707f4a4e06c6b
Harry Stead <harrystead9@gmail.com>
2020-11-22 10:44:29 +0000

    set up table with for loop and bootstrap

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+for (let i = 9; i < 18; i++) {
+    //loop through the to create a row and 4 columns for each hour between 9-5.
+    //so var = i represents the hour.
+    //give each column the id of the hour it represents - e.g, first column is 9am, and set 
+  
+      // create a row
+      var row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+      // create a column
+      //use bootstrap to display the table.
+      var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>'); //
+
+      //create column 2
+      var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
+     
+      //create column 3
+      var col3 = $(`<div class="col-sm-1"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`); 
+      //fontawesome to grab save and remove icons.
+      
+      // append col to row
+      row.append(col1);
+      row.append(col2);
+      row.append(col3);
+
+      // last step add rows to container
+      $(".container").append(row);
+
+  }
