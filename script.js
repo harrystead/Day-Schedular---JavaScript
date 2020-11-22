2dbfbe7c363c234ecf6b27f1518877964b1fb4a6
Harry Stead <harrystead9@gmail.com>
2020-11-22 10:49:40 +0000

    update colors, set interval, call functions

--- script.js
+++ script.js
@@ -43,7 +43,44 @@ for (let i = 9; i < 18; i++) {
       // last step add rows to container
       $(".container").append(row);
 
+      getLocalStorage(i);
+
   }
+
+  
+  //display am or pm depending on the time of day - and then link to first column, so that time displays alongside am/pm.
+  function formatAMPM(hours) {
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    return hours + ampm;
+}
+
+formatAMPM(); //call function
+
+function updateColors(){
+    var currentTime = new Date().getHours();
+    //get current hours.
+    for (var i = 9; i < 18; i++) { 
+      //loop through hours. 
+    console.log(currentTime, $(`#${i}`).data("time"));
+     if ($(`#${i}`).data("time") == currentTime){
+        $(`#text${i}`).addClass( "present");
+        //if current timer is equal to a value in the loop - set class to present.
+    } else if (currentTime < $(`#${i}`).data("time")) {
+        $(`#text${i}`).addClass( "future");
+    }
+    //if current time is less than value in loop, add class of future.
+}
+
+//style using css for present and future classes - changing colors when time changes.
+
+}
+
+//set timer - real time. and call function alongside with it. 
+setInterval(function() {
+updateColors();
+}, 1000);
 
   var saveBtn = $('.saveBtn');
 saveBtn.on('click', function(){
