04d8f49b35a2c8f559235ab03615a2ccfb5a0432
Harry Stead <harrystead9@gmail.com>
2020-11-22 10:47:55 +0000

    get item in local storage, clear btn, set date

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
 
-
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+  }
 
 $( document ).ready(function() {
+
+    var clearBtn = $(`<button>`);
+    clearBtn.addClass("clear-button")
+    clearBtn.text("Clear All")
+    $("#clear-btn-div").append(clearBtn);
+  
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 for (let i = 9; i < 18; i++) {
     //loop through the to create a row and 4 columns for each hour between 9-5.
